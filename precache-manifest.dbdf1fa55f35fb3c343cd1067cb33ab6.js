61f31a363e3e9e52f87bd1e9e92898cc614c43ce
Jordan Davidson <jordan.davidson@arnoldclark.com>
2020-04-20 20:19:28 +0100

    Updates

---
+++ precache-manifest.dbdf1fa55f35fb3c343cd1067cb33ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1846e396ec277130c9cf4a60a5d97ef5",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "11b00433025b46faee84",
+    "url": "/covid19/static/css/main.2bf97e6c.chunk.css"
+  },
+  {
+    "revision": "9a167ddc3b3df9fce343",
+    "url": "/covid19/static/js/2.9b139d9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.9b139d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b00433025b46faee84",
+    "url": "/covid19/static/js/main.a996ec6c.chunk.js"
+  },
+  {
+    "revision": "cdfcea41dfab553d9d0f",
+    "url": "/covid19/static/js/runtime-main.9f6017dd.js"
+  }
+]);
