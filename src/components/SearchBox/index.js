8f64e48172a47d3ef8c9ccf383567ac032c9ddaa
Jordan Davidson <jordan.davidson@arnoldclark.com>
2020-04-20 20:45:27 +0100

    lowercase search letters

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function SearchBox({ onSearch }) {
   function handleSearch(event) {
-    onSearch(event.target.value);
+    console.log(event.target.value);
+    onSearch(event.target.value.toLowerCase());
   }
 
   return <input placeholder="Search..." onKeyUp={handleSearch} />;
